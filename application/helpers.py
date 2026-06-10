76ca55e9b0dd7ad6597d9d65ac0c74fd1ebefbac
bunnnnyyyy22 <125729816+bunnnnyyyy22@users.noreply.github.com>
2026-06-11 07:34:30 +0800

    Update GitHub user in repository URL

--- application/helpers.py
+++ application/helpers.py
@@ -73,7 +73,7 @@ def get_repositories() -> list:
 
     github_token = os.environ["GITHUB_ACCESS"]
 
-    url = "https://api.github.com/users/geogeolo/repos"
+    url = "https://api.github.com/users/bunnnnyyyy22/repos"
     params = {"per_page": 1000}
     headers = {"Authorization": f"token {github_token}"}
 
